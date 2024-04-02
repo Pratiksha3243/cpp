06522b0615b7fd78e4928399351c2a6914ff2981
Pratiksha3243 <165653381+Pratiksha3243@users.noreply.github.com>
2024-04-02 10:26:36 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1,num2,sum;
+  cout<<"enter the num1:"<<endl;
+  cin>>num1;
+  cout<<"enter the num2:"<<endl;
+  cin>>num2;
+  sum=num1+num2;
+  cout<<"sum="<<sum;
+  return 0;
+}
